6ec5a802e04c353370e0a7eb442d5b02b6ebb15e
joeuong <38801460+joeuong@users.noreply.github.com>
2022-02-09 18:45:53 -0500

    fixed issue with 4th character

--- wordle.py
+++ wordle.py
@@ -14,15 +14,15 @@ with open('wordle_py.csv') as database:
 today = today.strftime('%Y-%m-%d')
 word_1 = date_word_dict[today]
 word_1 = word_1.upper()
-guess = 1
+guess = 0
 
 used_letters = []
 letter_in_word = []
 
-while guess < 6:
-    print(f'Guess {guess} ---------------------------------------- ')
+while guess < 5:
+    print(f'Guess {guess+1} ----------------------------')
 
-    word_1_index = 1
+    word_1_index = 0
     word_1_letters_dict = {}
     for letter in word_1:
         word_1_letters_dict[word_1_index] = letter.upper()
@@ -31,7 +31,7 @@ while guess < 6:
     user_guess = input(f'Enter a word: ')
     user_guess = user_guess.upper()
 
-    user_guess_index = 1
+    user_guess_index = 0
     user_guess_letters_dict = {}
 
     for letter in user_guess:
@@ -44,30 +44,23 @@ while guess < 6:
         continue
 
     if user_guess.lower() not in date_word_dict.values():
-        print('Needs to be real word')
+        print('Needs to be real 5-letter, non-plural word')
         continue
     
     if user_guess == word_1:
-        print(f'Congratulations, you guessed the word on Guess #{guess}')
+        print(f'Congratulations, you guessed the word on Guess #{guess+1}')
         break
-    
-    else:
-        a = 1
-        while a < 6:
-            if user_guess_letters_dict[a] == word_1_letters_dict[a]:
-                print(f'Letter {user_guess_letters_dict[a]} is in the right spot')
-                a += 1
-                continue
-            elif user_guess_letters_dict[a] in word_1_letters_dict.values():
-                print(f'Letter {user_guess_letters_dict[a]} is in the word')
-                del word_1_letters_dict[a]
-                a += 1
-                continue
-            else:
-                a += 1
-                continue
+
+    for k in user_guess_letters_dict:
+    #todo - if there is a double-letter, display proper # of times
+        if user_guess_letters_dict[k] in word_1_letters_dict.values():
+            print(f'Letter {user_guess_letters_dict[k]} is in the word')
+
+    for i in user_guess_letters_dict:
+        if user_guess_letters_dict[i] == word_1_letters_dict[i]:
+            print(f'Letter {user_guess_letters_dict[i]} is in the right spot')
+
     guess += 1
-    sleep(2)
 
 print(f'The word was {word_1}.')
 print('End of Game')
